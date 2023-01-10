210fd2cbab8f28290edccdc66b24b06bde70c37b
shellby.miller <shellby.miller@gmail.com>
2023-01-09 21:37:54 -0500

    set app to listen on port 3001

--- Develop/server.js
+++ Develop/server.js
@@ -11,3 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+//Listener
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
