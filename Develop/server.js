0789217bb98cc08353dedd000fe8be4f02390f68
shellby.miller <shellby.miller@gmail.com>
2023-01-09 21:54:27 -0500

    removed routes file

--- Develop/server.js
+++ Develop/server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 //Data parsing
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 app.use(express.static('public'));
+app.use(express.json());
 
 //Listener
 app.listen(PORT, () => {
