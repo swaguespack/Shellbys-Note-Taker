9872250803888f1c0a8415d84ba103b576d57ca5
shellby.miller <shellby.miller@gmail.com>
2023-01-09 21:33:23 -0500

    added data parsing to app

--- Develop/server.js
+++ Develop/server.js
@@ -7,3 +7,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
