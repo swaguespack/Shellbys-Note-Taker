afc09e802a6be1b23dce4efabf42801e97c6c189
shellby.miller <shellby.miller@gmail.com>
2023-01-09 21:25:03 -0500

    updated server.js to include dependencies and initialize express app

---
+++ Develop/server.js
@@ -0,0 +1,9 @@
+//Include modules needed for this application
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+//Initialize the app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
