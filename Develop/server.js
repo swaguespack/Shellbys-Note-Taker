6810dd7964eaeda1a34924676ec33b2eee8c333b
shellby.miller <shellby.miller@gmail.com>
2023-01-09 21:48:40 -0500

    updated dir name for data parsing

--- Develop/server.js
+++ Develop/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 //Data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
 //Listener
 app.listen(PORT, () => {
