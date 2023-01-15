cb29c4d8445a6285a762686f92c17b45fb12fc5b
shellby.miller <shellby.miller@gmail.com>
2023-01-15 12:19:02 -0500

    added HTML routes & commented code in server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Set up app to handle data parsing, serve static files in directory 'public', parse incoming JSON requests to put in req.body
+//Middleware methods to deal with incoming data in req.body & serve static files from '/public'
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
 app.use(express.json());
+app.use(express.static('public'));
 
 //API/notes post route
 app.post('/api/notes', (req, res) => {
@@ -41,25 +41,34 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 });
 
-//Retreive note with specific ID
+//API Routes
+//GET route to get note with specific id 
 app.get('api/notes/:id', (req, res) =>{
+  //Return JSON using res.json()
   res.json(notes[req.params.id]);
 });
 
+//GET route to get notes
 app.get('/api/notes', (req, res) => {
+  //Read db.json file
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //If callback function error, throw error
     if (err) throw err;
+    //Store data from callback function in notes variable
     var notes = JSON.parse(data);
+    //Return JSON of notes
     res.json(notes);
   });
 });
 
+//HTML Routes
+//GET route to return absolute file paths for notes.html & index.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile('/public/notes.html',{root:__dirname});
 });
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '/public/index.html'));
+   res.sendFile('/public/index.html',{root:__dirname});
 });
 
 //Listener to return data inside app.listen callback
