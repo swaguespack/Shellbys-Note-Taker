152571580e354815f9018f28b68a61328425b03e
shellby.miller <shellby.miller@gmail.com>
2023-01-15 12:59:10 -0500

    added delete route and comments

--- server.js
+++ server.js
@@ -7,35 +7,48 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Middleware methods to deal with incoming data in req.body & serve static files from '/public'
+//Middleware to deal with incoming data in req.body & serve static files from '/public'
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-//API/notes post route
+//Respond to POST request to add notes
 app.post('/api/notes', (req, res) => {
+  //Read db.json file
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //If callback function error, throw error
     if (err) throw err;
+    //Store data from callback function in notes variable
     var notes = JSON.parse(data);
+    //Access client side data from req.body and store in newNote variable
     let newNote = req.body;
+    //Assign random id
     newNote.id = Math.floor(Math.random() * 5000);
+    //Add new note element to end of notes array
     notes.push(newNote);
+  //Write updated notes back to db.json file
   fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
       res.json(newNote);
+      //Log new note titles to terminal
       return console.log("Added new note: " + newNote.title);
   });
   }); 
 });
 
-//API/notes delete route deletes note with specific ID
+//Respond to DELETE request to delete note with specific ID
 app.delete('/api/notes/:id', (req, res) => {
+  //Read db.json file
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //If callback function error, throw error
     if (err) throw err;
+    //Store data from callback function in notes variable
     let notes = JSON.parse(data);
+    //Filter by note id & store in newNotes
     const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
-  
+  //Write updated notes back to db.json file
   fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
     res.json({msg: 'successfully'});
+    return console.log("Deleted note successfully");
 
   });
 });
@@ -62,11 +75,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 //HTML Routes
-//GET route to return absolute file paths for notes.html & index.html
+//GET route for notes page
 app.get('/notes', (req, res) => {
     res.sendFile('/public/notes.html',{root:__dirname});
 });
-
+//GET route for homepage
 app.get('*', (req, res) => {
    res.sendFile('/public/index.html',{root:__dirname});
 });
